8eb9533f38c6d9718a0570eb1263f19c0bc0fd23
Sudhish Meenakshisundaram <sudhish.3120@gmail.com>
2020-09-07 12:25:58 -0400

    upload dice roller

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+var randomNumber1=Math.floor((Math.random()*6)+1);
+
+if (randomNumber1===1) {
+  document.querySelector(".img1").setAttribute("src","images/dice1.png");
+}else if (randomNumber1===2) {
+  document.querySelector(".img1").setAttribute("src","images/dice2.png");
+}else if (randomNumber1===3) {
+  document.querySelector(".img1").setAttribute("src","images/dice3.png");
+}else if (randomNumber1===4) {
+  document.querySelector(".img1").setAttribute("src","images/dice4.png");
+}else if (randomNumber1===5) {
+  document.querySelector(".img1").setAttribute("src","images/dice5.png");
+}else if (randomNumber1===6) {
+  document.querySelector(".img1").setAttribute("src","images/dice6.png");
+}
+
+var randomNumber2=Math.floor((Math.random()*6)+1);
+
+if (randomNumber2===1) {
+  document.querySelector(".img2").setAttribute("src","images/dice1.png");
+}else if (randomNumber2===2) {
+  document.querySelector(".img2").setAttribute("src","images/dice2.png");
+}else if (randomNumber2===3) {
+  document.querySelector(".img2").setAttribute("src","images/dice3.png");
+}else if (randomNumber2===4) {
+  document.querySelector(".img2").setAttribute("src","images/dice4.png");
+}else if (randomNumber2===5) {
+  document.querySelector(".img2").setAttribute("src","images/dice5.png");
+}else if (randomNumber2===6) {
+  document.querySelector(".img2").setAttribute("src","images/dice6.png");
+}
+
+var heading=document.querySelector("h1");
+if (randomNumber1>randomNumber2) {
+  heading.innerHTML="🚩Player 1 Wins";
+}else if(randomNumber2>randomNumber1) {
+  heading.innerHTML="Player 2 Wins 🚩";
+}else if(randomNumber1===randomNumber2) {
+  heading.innerHTML="Draw!";
+}
